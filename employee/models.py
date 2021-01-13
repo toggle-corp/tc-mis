0e632077dd53ade635a50b9453b76f6ef29df1bc
nirojtogglecorp <niroj.dyola@togglecorp.com>
2021-01-13 16:58:03 +0545

    Added Validation to PhoneNumber

--- employee/models.py
+++ employee/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils.html import mark_safe
 import datetime
 from django_currentuser.middleware import get_current_authenticated_user
+from django.core.validators import RegexValidator
 # Create your models here.
 
 
@@ -26,21 +27,27 @@ class Employee(models.Model):
     dob = models.DateField(verbose_name='Date of Birth')
     gender = models.IntegerField(choices=GENDER)
     email = models.EmailField(max_length=255, unique=True)
-    phone_number = models.BigIntegerField()
+    phone_number = models.CharField(max_length=16, validators=[
+        RegexValidator(
+            regex=r'^\+977\s-?\d{10}$',
+            message="Phone number must be entered in the format '+977 9999999999'."
+        ),
+    ],)
     address = models.CharField(max_length=255)
     pan_no = models.CharField(max_length=255, unique=True)
     citizenship_no = models.CharField(max_length=255, unique=True)
-    designation = models.ForeignKey(
-        Designation, on_delete=models.DO_NOTHING)
+    designation = models.ForeignKey(Designation, on_delete=models.DO_NOTHING)
     join_date = models.DateField()
     picture = models.ImageField(upload_to='images/')
     pan_no_document = models.ImageField(upload_to='images/')
     cititzen_document = models.ImageField(upload_to='images/')
     status = models.IntegerField(choices=STATUSES)
     createdBy = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, related_name="createdBy")
+        User, on_delete=models.DO_NOTHING, related_name="createdBy"
+    )
     updatedBy = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, related_name="updatedBy")
+        User, on_delete=models.DO_NOTHING, related_name="updatedBy"
+    )
     createdAt = models.DateTimeField(auto_now=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
