05eab3b3e6b19d1108c93074d6735ec087b695d8
nirojtogglecorp <niroj.dyola@togglecorp.com>
2021-01-18 11:03:58 +0545

    Followed Naming Convention

--- employee/models.py
+++ employee/models.py
@@ -36,20 +36,20 @@ class Employee(models.Model):
     address = models.CharField(max_length=255)
     pan_no = models.CharField(max_length=255, unique=True)
     citizenship_no = models.CharField(max_length=255, unique=True)
-    designation = models.ForeignKey(Designation, on_delete=models.DO_NOTHING)
+    designation = models.ForeignKey(Designation, on_delete=models.PROTECT)
     join_date = models.DateField()
     picture = models.ImageField(upload_to='images/pictures/')
     pan_no_document = models.ImageField(upload_to='images/pans/')
-    cititzen_document = models.ImageField(upload_to='images/citizenships/')
+    citizenship_document = models.ImageField(upload_to='images/citizenships/')
     status = models.IntegerField(choices=STATUSES)
-    createdBy = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, related_name="createdBy"
+    created_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="created_by"
     )
-    updatedBy = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, related_name="updatedBy"
+    updated_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="updated_by"
     )
-    createdAt = models.DateTimeField(auto_now=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.fullname
