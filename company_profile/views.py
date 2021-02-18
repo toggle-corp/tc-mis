9662879d19a1dd0caca9d59ee0f98f47124189d3
nirojtogglecorp <niroj.dyola@togglecorp.com>
2021-02-18 17:41:01 +0545

    Added employees details

--- company_profile/views.py
+++ company_profile/views.py
@@ -5,9 +5,13 @@ from employee.models import Employee
 
 
 def index(request):
-    employees = Employee.objects.values('department').annotate(count=Count('id')).filter(is_active=True)
+    employee_departments = Employee.objects.values('department__name').annotate(count=Count('id')).filter(
+        is_active=True).exclude(department__isnull=True)
+    employees = Employee.objects.filter(is_active=True).exclude(department__isnull=True).order_by('-join_date')
     context = {
-        "employees": employees
+        "employee_departments": employee_departments,
+        "employees": employees,
+        "title": "ToggleCorp Solutions Pvt. Ltd."
     }
 
     return render(request, "changelist.html", context)
