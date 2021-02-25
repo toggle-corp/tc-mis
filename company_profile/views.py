02dbcd0c18809b31de69c8dd5880d1519e74bdae
nirojtogglecorp <niroj.dyola@togglecorp.com>
2021-02-25 13:46:46 +0545

    Removed unused code

--- company_profile/views.py
+++ company_profile/views.py
@@ -7,11 +7,12 @@ from employee.models import Employee
 
 
 def index(request):
-    # employee_departments = Employee.objects.values('department__name').annotate(count=Count('id')).filter(
-    #     is_active=True).exclude(department__isnull=True)
-    employee_departments = Department.objects.all().annotate(
-        employee_count=Count('employee_department', filter=Q(employee_department__is_active=True)))
-    print(employee_departments)
+    employee_departments = Department.objects.annotate(
+        employee_count=Count(
+            'employee_department',
+            filter=Q(employee_department__is_active=True)
+        )
+    )
     employees = Employee.objects.filter(is_active=True).exclude(department__isnull=True).order_by('-join_date')
     context = {
         "employee_departments": employee_departments,
