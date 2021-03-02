ddcd0a8dca7e4c418b0e94695156e37d53f7a74e
nirojtogglecorp <niroj.dyola@togglecorp.com>
2021-03-02 12:40:51 +0545

    Presave signal removed

--- salary/models.py
+++ salary/models.py
@@ -1,6 +1,7 @@
 from django.core.mail import BadHeaderError, EmailMultiAlternatives
 from django.db import models
-from django.db.models.signals import pre_save, post_save
+from django.db import transaction
+from django.db.models.signals import post_save
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
@@ -20,10 +21,9 @@ class Salary(models.Model):
     def __str__(self):
         return f'{self.employee.fullname} - NRs {self.net_salary}'
 
-
-def pre_salary_created_signal(sender, instance, *args, **kwargs):
-    net_salary = (instance.basic_salary + instance.allowance + instance.gross_salary) * instance.tax / 100
-    instance.net_salary = net_salary
+    def save(self, *args, **kwargs):
+        self.net_salary = (self.basic_salary + self.allowance + self.gross_salary) * self.tax / 100
+        return super(Salary, self).save(*args, **kwargs)
 
 
 def post_salary_created_signal(sender, instance, created, **kwargs):
@@ -32,7 +32,12 @@ def post_salary_created_signal(sender, instance, created, **kwargs):
         html_content = render_to_string("salary_email_template.html", {'data': instance, 'content': content})
         subject = f'Salary Review - {instance.employee.fullname}'
         text_content = strip_tags(html_content)
-        send_salary_email(subject, text_content, instance.employee.email, html_content)
+        transaction.on_commit(lambda: send_salary_email(
+            subject,
+            text_content,
+            instance.employee.email,
+            html_content
+        ))
 
 
 def send_salary_email(subject, content, to, html_content):
@@ -49,5 +54,4 @@ def send_salary_email(subject, content, to, html_content):
         return HttpResponse('Invalid header found.')
 
 
-pre_save.connect(pre_salary_created_signal, sender=Salary)
 post_save.connect(post_salary_created_signal, sender=Salary)
